29d63cf8d1d476a3480dc8718e8a9eb71e52f56f
Muhammad-Saeed-0 <muhammad01111417159@gmail.com>
2023-03-01 16:11:03 +0200

    rollDice

--- app/src/main/java/com/dragon/apps/diceroller/MainActivity.kt
+++ app/src/main/java/com/dragon/apps/diceroller/MainActivity.kt
@@ -3,7 +3,9 @@ package com.dragon.apps.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
@@ -14,14 +16,28 @@ class MainActivity : AppCompatActivity() {
         val rollBtn : Button = findViewById(R.id.roll_button)
 
         rollBtn.setOnClickListener{
-            //Toast.makeText(this, "Roll btn is clicked", Toast.LENGTH_SHORT).show()
 
-            rollDice()
+            val diceNumber :Int = Random.nextInt(6) + 1
+            Toast.makeText(this, diceNumber.toString(), Toast.LENGTH_SHORT).show()
+
+            roll(diceNumber)
         }
     }
 
-    private fun rollDice() {
-        val rollResult : TextView = findViewById(R.id.roll_textView)
-        rollResult.text = (Random.nextInt(6) + 1).toString()
+    private fun roll(diceNumber :Int) {
+        val rollDice : ImageView = findViewById(R.id.dice)
+
+        val drawable = when(diceNumber)
+        {
+            1 -> R.drawable.dice_1
+            2 -> R.drawable.dice_2
+            3 -> R.drawable.dice_3
+            4 -> R.drawable.dice_4
+            5 -> R.drawable.dice_5
+            6 -> R.drawable.dice_6
+            else -> R.drawable.empty_dice
+        }
+
+        rollDice.setImageResource(drawable)
     }
 }
